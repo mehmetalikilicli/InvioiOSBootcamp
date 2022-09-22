bdbe44e8326b5ef9eb80fa7dcc543f136562db60
Mehmet Ali <58392243+mehmetalikilicli@users.noreply.github.com>
2022-09-22 10:09:57 +0300

    Finish Section1 And Homework1

---
+++ Homeworks/Homework1/Homework1.playground/Contents.swift
@@ -0,0 +1,39 @@
+//INVIO IOS BOOTCAMP
+//MEHMET ALİ KILIÇLI
+//HOMEWORK 1
+
+import UIKit
+
+// PROCESS
+let processNo : Double = 3.4
+let studentNo : Int = 456
+var bookNo : Int = 789
+var bookBoughtDate : String = "2022-10-21"
+var bookGaveDate : String = "2022-10-22"
+
+//STUDENT
+var studentNo2 : String = "1092"
+var studentName : String = "Mehmet Ali"
+var studentSurname : String = "Kilicli"
+var gender : String = "Male"
+var dateOfBirth : String = "1998-04-13"
+var isGraduate : Bool = true
+
+//BOOK
+var bookNo2 : String = "789"
+var isbnno : Int = 123
+var bookName : String = "Ucurtma Avcisi"
+var authorNo : Int = 987
+var genreOfBook : String = "Novel"
+let numberOfPages : Int = 765
+let scoreOutOf10 : Double = 8.7
+
+//AUTHOR
+var authorNo2 : Int = 876
+var authorName : String = "Ahmet"
+var authorSurname : String = "Kilci"
+
+//GENRE
+var genreNo : Int = 2
+var genreName : String = "Novel"
+
